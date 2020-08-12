49342c88def915f9d7988dc8a14619d44abcebef
diana <dianagiosan@yahoo.com>
2020-08-12 16:45:42 +0300

    Refactorization. Moved a lot of functionalities from the main class. Created several methods to perform the former main class functionalities.

--- src/main/java/Utils/UsageMessage.java
+++ src/main/java/Utils/UsageMessage.java
@@ -1,8 +1,8 @@
 package Utils;
 
 public class UsageMessage {
-	private static final String usageMessage = "Usage : filepath/url --options.\n Options: -remote: determines that " +
-		"the path given is the url for a remote repo; -branch branch name specify branch to get commit messages from.";
+	private static final String usageMessage = "Usage : 'filepath/url' --options.\n Options: -remote: determines that " +
+		"the path given is the url for a remote repo; -branch 'branchname' specify branch to get commit messages from.";
 	
 	public static String getUsageMessage() {
 		return usageMessage;
